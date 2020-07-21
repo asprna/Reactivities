d5397f665494e62e57e1773681e0d37a33c7d10b
Administrator - Asanka Nawagamuwage <nawagamuwagea-admin@corporate.local>
2020-07-21 16:56:25 +1000

    Edit and Delete

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -11,6 +11,10 @@ interface IProps {
   selectedActivity: IActivity | null;
   editMode: boolean;
   setEditMode: (editMode: boolean) => void;
+  setSelectedActivity: (activity: IActivity | null) => void;
+  createActivity: (activity: IActivity) => void;
+  editActivity: (activity: IActivity) => void;
+  deleteActivity: (id: string) => void;
 }
 
 const ActivityDashboard: React.FC<IProps> = ({
@@ -18,16 +22,20 @@ const ActivityDashboard: React.FC<IProps> = ({
   selectActivity,
   selectedActivity,
   editMode,
-  setEditMode
+  setEditMode,
+  setSelectedActivity,
+  createActivity,
+  editActivity,
+  deleteActivity
 }) => {
   return (
     <Grid>
       <Grid.Column width={10}>
-        <ActivityList activities={activities} selectActivity={selectActivity} />
+        <ActivityList activities={activities} selectActivity={selectActivity} deleteActivity={deleteActivity} />
       </Grid.Column>
       <Grid.Column width={6}>
-        {selectedActivity && !editMode && (<ActivityDetails activity={selectedActivity} setEditMode={setEditMode}/>)}
-        {editMode && (<ActivityForm />)}
+        {selectedActivity && !editMode && (<ActivityDetails activity={selectedActivity} setEditMode={setEditMode} setSelectedActivity={setSelectedActivity} />)}
+        {editMode && (<ActivityForm key={(selectedActivity && selectedActivity.id) || 0} setEditMode={setEditMode} activity={selectedActivity!} createActivity={createActivity} editActivity={editActivity} />)}
       </Grid.Column>
     </Grid>
   );
